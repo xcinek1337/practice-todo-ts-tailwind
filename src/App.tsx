239cae11a71bc866bf71cd1dc4b8853c1adea6ab
Marcin <xcinek1337@wp.pl>
2024-01-31 11:59:25 +0100

    Created form component passing input value to parent via props

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 
 import TodoList from './components/TodoList';
+import NewTodo from './components/NewTodo';
+
+
+
 
 const App: React.FC = () => {
   const todos = [{ id: 't1', text: 'finish the curse' }];
+  const todoAddHandler = (text: string) => {
+		console.log(text);
+	};
+  
 	return (
 		<div className='App'>
-      {/* a component that adds todos */}
+			<NewTodo onAddTodo={todoAddHandler} />
 			<TodoList items={todos} />
 		</div>
 	);
---
+++ src/components/NewTodo.tsx
@@ -0,0 +1,30 @@
+import React, { useRef } from 'react';
+
+// type == interface
+type newTodoProps = {
+	onAddTodo: (todoText: string) => void;
+};
+
+const NewTodo: React.FC<newTodoProps> = (props) => {
+	const textInputRef = useRef<HTMLInputElement>(null);
+	const todoSubmitHandler = (event: React.FormEvent) => {
+		event.preventDefault();
+		const enteredText = textInputRef.current!.value;
+		props.onAddTodo(enteredText);
+	};
+
+	return (
+		<form onSubmit={todoSubmitHandler}>
+			<div>
+				<label htmlFor='todo-text'>Todo Text</label>
+				<input
+					ref={textInputRef}
+					type='text'
+					id='todo-text'
+				/>
+			</div>
+			<button type='submit'>ADD TODO</button>
+		</form>
+	);
+};
+export default NewTodo;
