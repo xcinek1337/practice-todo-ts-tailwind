6fe8ad9334ee8b37202df91bb4302a8b536fca73
Marcin <xcinek1337@wp.pl>
2024-01-31 11:23:41 +0100

    adding generic type for todoList component

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import TodoList from './components/TodoList';
+
+const App: React.FC = () => {
+  const todos = [{ id: 't1', text: 'finish the curse' }];
+	return (
+		<div className='App'>
+      {/* a component that adds todos */}
+			<TodoList items={todos} />
+		</div>
+	);
+};
 
 export default App;
