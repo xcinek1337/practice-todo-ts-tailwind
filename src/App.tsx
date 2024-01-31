63bf9de34471118deaac1e659cdf21a36749a648
Marcin <xcinek1337@wp.pl>
2024-01-31 12:24:54 +0100

    delete todo function implemetned

--- src/App.tsx
+++ src/App.tsx
@@ -10,11 +10,18 @@ const App: React.FC = () => {
   const todoAddHandler = (text: string) => {
     setTodos((prevTodos) => [...prevTodos, { id: Math.random().toString(), text: text }]);
 	};
+
+  const todoDeleteHandler = (todoId: string) => {
+		setTodos((prevTodos) => {
+			return prevTodos.filter((todo) => todo.id !== todoId);
+	
+		});
+	};
   
 	return (
 		<div className='App'>
 			<NewTodo onAddTodo={todoAddHandler} />
-			<TodoList items={todos} />
+			<TodoList onDeleteTodo={todoDeleteHandler} items={todos} />
 		</div>
 	);
 };
